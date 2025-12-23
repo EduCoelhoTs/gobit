6e6221b54e8b59fbf548b3eb47f2121036b16f99
Eduardo Coelho <eduardo.coelho@pge.ce.gov.br>
2025-12-23 09:42:34 -0300

    feat: creating api initial strucure

---
+++ internal/api/user_handler.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) HandleLogin(w http.ResponseWriter, r *http.Request) {
+	panic("NOT implemented")
+}
+
+func (api *Api) HandleSignup(w http.ResponseWriter, r *http.Request) {
+	panic("NOT implemented")
+}
+
+func (api *Api) HandleLogout(w http.ResponseWriter, r *http.Request) {
+	panic("NOT implemented")
+}
